fad8ea714223b5a5a766d5a3c455f5459324f424
fell0w-sch0l4r <angelcagiza@gmail.com>
2020-05-27 04:22:11 +0100

    ex 1 e 2

---
+++ EX001 - Listagem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+printf("<<< EX001 - Listagem >>>\n");
+printf("\n Listagem de Alunos");
+printf("\n Nome\t\tNota");
+printf("\n ----------------------");
+printf("\n Ana Beatriz\t8.5");
+printf("\n Bianca Martins 9.0");
+printf("\n Cláudio Sá\t5.5");
+printf("\n Giovana Silva  7.5");
+}
