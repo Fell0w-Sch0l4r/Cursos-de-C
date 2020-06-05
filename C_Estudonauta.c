5855cb8d06506e5e749829a0815152d38ec2e2a6
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-06-05 14:31:47 +0100

    ,

--- C_Estudonauta.c
+++ C_Estudonauta.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-#include <locale.h>
-void main(){
-setlocale(LC_ALL, "Portuguese");
-printf("C é");
-printf("\n\"SUPER\"");
-printf("\nFÁCIL!");
-}
+
---
+++ cmake-build-debug/CMakeFiles/Cursos_de_C.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cursos_de_C.dir/main.c.obj"
+  "CMakeFiles/Cursos_de_C.dir/teste.c.obj"
+  "Cursos_de_C.exe"
+  "Cursos_de_C.exe.manifest"
+  "Cursos_de_C.pdb"
+  "libCursos_de_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cursos_de_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
