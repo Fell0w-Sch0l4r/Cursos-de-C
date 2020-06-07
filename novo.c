3f95f1c762f84a1da3c20c1636e7294ca1339077
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-06-07 12:54:01 +0100

    n

--- novo.c
+++ novo.c
@@ -1,4 +1,10 @@
-//
-// Created by hp on 06/06/2020.
-//
-
+#import <stdio.h>
+#import <locale.h>
+void main(){
+    setlocale(LC_ALL,"Portuguese");
+    char re[30];
+    printf("Digite o seu nome: ");
+    setlocale(LC_ALL,"Portuguese");
+    gets(re);
+    printf("O seu nome é %s",re);
+}
